45eb008eccab7b15d5cb8fc574d4399e0fa9aae2
Aaron McPeek <arn1313@comcast.net>
2017-06-06 16:40:31 -0700

    update 3?

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 'use strict';
-var dogCat = confirm("Do you think i am a cat guy?");
-
-alert ("Hey is this thing on");
+var dogCat = prompt("Lets play a game about a subject i like, ME! Do you think i am a cat guy?").toUpperCase();
+  if (dogCat === "YES"){
+    alert("I am generally a dog guy but i do have an elderly kitty at home.");
+    console.log("I am generally a dog guy but i do have an elderly kitty at home.");
+  } else{
+    alert("You are right! Although i do have a cat at home");
+    console.log("You are right! Although i do have a cat at home");
+  }
+var likeGames = prompt("Do you think i like video games?").toUpperCase();
+  if (likeGames === "YES"){
+    alert("I Loooooooove video games!");
+    console.log("I Loooooooove video games!");
+  } else {
+    alert("Nope, i am an avid gamer.");
+    console.log("Nope, i am an avid gamer.");
+  }
+  var favColor = prompt("Do i like the color green?").toUpperCase();
+  if (favColor === "YES"){
+    alert("Your are correct! Especially love it in a plaid or flannel");
+    console.log("Your are correct! Especially love it in a plaid or flannel");
+  } else {
+    alert("Nope, it is one of my favorite colors.");
+    console.log("Nope, it is one of my favorite colors.");
+  }
+var vileFruit = prompt("Do you think i am a fan of the tomatoe?").toUpperCase();
+  if (vileFruit === "YES"){
+      alert("Ye Gads no.... I don't touch the vile fruit!");
+      console.log("Ye Gads no.... I don't touch the vile fruit!");
+  } else {
+    alert("You are correct!! I truly hates them");
+    console.log("You are correct!! I truly hates them");
+  }
+  var goodCode = prompt("Do you think i will make a good coder?").toUpperCase();
+    if (goodCode === "YES"){
+      alert("Thank You!! I appreciate your support!");
+      console.log("Thank You!! I appreciate your support!");
+    } else {
+      alert("I am making you a batch of frowny brownies");
+      console.log("I am making you a batch of frowny brownies");
+    }
