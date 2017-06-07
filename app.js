6efa5bd728437426f5aa59a2148ac290edfa6d93
Aaron McPeek <arn1313@comcast.net>
2017-06-06 17:19:28 -0700

    update 4 now with ||

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 'use strict';
 var dogCat = prompt("Lets play a game about a subject i like, ME! Do you think i am a cat guy?").toUpperCase();
-  if (dogCat === "YES"){
+  if (dogCat === "YES" || dogCat === "Y"){
     alert("I am generally a dog guy but i do have an elderly kitty at home.");
     console.log("I am generally a dog guy but i do have an elderly kitty at home.");
-  } else{
+  } else if (dogCat === "NO" || dogCat === "N"){
     alert("You are right! Although i do have a cat at home");
     console.log("You are right! Although i do have a cat at home");
+  } else {
+    alert("Sorry i didn't understand");
   }
 var likeGames = prompt("Do you think i like video games?").toUpperCase();
-  if (likeGames === "YES"){
+  if (likeGames === "YES" || likeGames === "Y"){
     alert("I Loooooooove video games!");
     console.log("I Loooooooove video games!");
-  } else {
+  } else if (likeGames === "NO" || likeGames === "N"){
     alert("Nope, i am an avid gamer.");
     console.log("Nope, i am an avid gamer.");
+  } else {
+    alert("Sorry i didn't understand");
   }
   var favColor = prompt("Do i like the color green?").toUpperCase();
-  if (favColor === "YES"){
+  if (favColor === "YES" || favColor === "Y"){
     alert("Your are correct! Especially love it in a plaid or flannel");
     console.log("Your are correct! Especially love it in a plaid or flannel");
-  } else {
+  } else if (favColor === "NO" || favColor === "N"){
     alert("Nope, it is one of my favorite colors.");
     console.log("Nope, it is one of my favorite colors.");
+  } else {
+    alert("Sorry i didn't understand");
   }
 var vileFruit = prompt("Do you think i am a fan of the tomatoe?").toUpperCase();
-  if (vileFruit === "YES"){
+  if (vileFruit === "YES" || vileFruit === "Y"){
       alert("Ye Gads no.... I don't touch the vile fruit!");
       console.log("Ye Gads no.... I don't touch the vile fruit!");
-  } else {
+  } else if (vileFruit === "NO" || vileFruit === "N"){
     alert("You are correct!! I truly hates them");
     console.log("You are correct!! I truly hates them");
+  } else {
+    alert("Sorry i didn't understand");
   }
   var goodCode = prompt("Do you think i will make a good coder?").toUpperCase();
-    if (goodCode === "YES"){
+    if (goodCode === "YES" || goodCode === "Y"){
       alert("Thank You!! I appreciate your support!");
       console.log("Thank You!! I appreciate your support!");
-    } else {
+    } else if (goodCode === "NO" || goodCode === "N"){
       alert("I am making you a batch of frowny brownies");
       console.log("I am making you a batch of frowny brownies");
+    } else {
+      alert("Sorry i didn't understand");
     }
